9d3391355b6da453d532e7bc11d0777a35a450e8
Voinalovych-Anastasiia <voinalovych.anastasiia@gmail.com>
2022-08-28 10:06:06 +0300

    Added the project files

---
+++ src/index.js
@@ -0,0 +1,120 @@
+function updateDate() {
+  let now = new Date();
+  let date = now.getDate();
+  let hours = now.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesdy",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[now.getDay()];
+
+  let today = document.querySelector(".day");
+  today.innerHTML = `${day} ${date}`;
+
+  let hour = document.querySelector(".hour");
+  hour.innerHTML = `${hours}:${minutes}`;
+}
+
+updateDate();
+
+let apiKey = "7e4d33361f2ae1188d23e9af28a43e43";
+let cityName = "Kyiv";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+function showWeather(response) {
+  let tempElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.main.temp);
+  tempElement.innerHTML = `${temperature}`;
+
+  let humidityElement = document.querySelector(".humidity");
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}`;
+
+  let windSpeedElement = document.querySelector(".windSpeed");
+  let windSpeed = 3.6 * response.data.wind.speed;
+  windSpeed = windSpeed.toFixed(1);
+  windSpeedElement.innerHTML = `Wind: ${windSpeed} km/h`;
+
+  let descriptionElement = document.querySelector(".description");
+  descriptionElement.innerHTML = `${response.data.weather[0].main}`;
+}
+
+axios.get(apiUrl).then(showWeather);
+
+function showCurrentWeather(response) {
+  let positionNameElement = document.querySelector("h1");
+  positionNameElement.innerHTML = `${response.data.name}`;
+
+  let tempElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.main.temp);
+  tempElement.innerHTML = `${temperature}`;
+
+  let humidityElement = document.querySelector(".humidity");
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}`;
+
+  let windSpeedElement = document.querySelector(".windSpeed");
+  let windSpeed = 3.6 * response.data.wind.speed;
+  windSpeed = windSpeed.toFixed(1);
+  windSpeedElement.innerHTML = `Wind: ${windSpeed} km/h`;
+
+  let descriptionElement = document.querySelector(".description");
+  descriptionElement.innerHTML = `${response.data.weather[0].main}`;
+}
+
+function findLocation(position) {
+  let latitude = position.coords.latitude;
+  let longitude = position.coords.longitude;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showCurrentWeather);
+}
+
+function getLocation() {
+  navigator.geolocation.getCurrentPosition(findLocation);
+}
+
+let locationButton = document.querySelector(".currentWeatherButton");
+locationButton.addEventListener("click", getLocation);
+
+function showSearchWeather(response) {
+  let tempElement = document.querySelector("#temperature");
+  let temperature = Math.round(response.data.main.temp);
+  tempElement.innerHTML = `${temperature}`;
+
+  let humidityElement = document.querySelector(".humidity");
+  humidityElement.innerHTML = `Humidity: ${response.data.main.humidity}`;
+
+  let windSpeedElement = document.querySelector(".windSpeed");
+  let windSpeed = 3.6 * response.data.wind.speed;
+  windSpeed = windSpeed.toFixed(1);
+  windSpeedElement.innerHTML = `Wind: ${windSpeed} km/h`;
+
+  let descriptionElement = document.querySelector(".description");
+  descriptionElement.innerHTML = `${response.data.weather[0].main}`;
+}
+
+function searchWeather(event) {
+  event.preventDefault();
+  let searchCity = document.querySelector("#search-city");
+  let city = document.querySelector("h1");
+  city.innerHTML = `${searchCity.value}`;
+  city = `${searchCity.value}`;
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(showSearchWeather);
+}
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchWeather);
